8e2659f4e0532e71a1b46b39f3496553ea2abc6f
nihaopaul <nihaopaul@gmail.com>
2013-03-26 00:24:17 +0800

    changed the admin form, removing errors by removing options.

--- app/scripts/controllers/approveApplication.js
+++ app/scripts/controllers/approveApplication.js
@@ -2,7 +2,17 @@
 
 angular.module('grahamApp.controllers.approveApplication', [])
   .controller('ApproveApplicationCtrl', function ($scope, $routeParams, $location, Application, PopupService) {
+
+
 		$scope.application = Application.get({applicationId: $routeParams.id});
+		$scope.memberships = [
+			{name:'(100元) One Month', months:'1', cost:'100'},
+			{name:'(250元) Three Months', months:'3', cost:'250'},
+			{name:'(450元) Six Months', months:'6', cost:'450'}
+		];
+
+
+
 		$scope.remove = function(id){
 			Application.remove({applicationId: id});
 			PopupService.close();
@@ -18,5 +28,5 @@ angular.module('grahamApp.controllers.approveApplication', [])
 				}
 			});
 		};
-		console.log($scope.application);
+		//console.log($scope.application);
 	});
