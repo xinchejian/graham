bcc3b67cc91d3981e8e83013163e2d27129f8962
nihaopaul <nihaopaul@nihaopaul.com>
2013-11-24 19:02:47 +0800

    upgraded angular to 1.2.2 stable, bower install

--- client/scripts/controllers/account.js
+++ client/scripts/controllers/account.js
@@ -3,17 +3,11 @@
 angular.module('grahamApp.controllers')
 
 
-	.controller('ViewAccountCtrl', function ($scope, Member, auth) {
+	.controller('ViewAccountCtrl', function ($scope, Member, auth, $location) {
 		$scope.member = Member.get({memberId: auth.currentUserId()});
 		$scope.updateMember = function() {
 			Member.updateMember($scope.member, function(u) {
 				$location.path('/account/success');
-			}), function(err){
-				if(err.data.error){
-					$scope.errorMsg = err.data.error;
-				}else {
-					$scope.errorMsg = 'Sorry! Failed to update password, please try again later.';
-				}
 			});
 		}
 
