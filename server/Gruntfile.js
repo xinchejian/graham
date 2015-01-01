07f2e5fbf1fdccef1d85502d64142c132bead2f1
Paul Adams <paul.adams@frogdesign.com>
2015-01-01 19:31:29 +0000

    included partials

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -133,8 +133,8 @@ module.exports = function (grunt) {
         files: {
           '<%= yeoman.dist %>/scripts/scripts.js': [
             '.tmp/scripts/*.js',
-            '<%= yeoman.app %>/scripts/*.js',
-            
+            // '<%= yeoman.app %>/scripts/*.js',
+
           ]
         }
       }
@@ -188,7 +188,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.app %>',
-          src: ['*.html', 'views/*.html'],
+          src: ['*.html', 'views/*.html', 'partials/*.html'],
           dest: '<%= yeoman.dist %>'
         }]
       }
